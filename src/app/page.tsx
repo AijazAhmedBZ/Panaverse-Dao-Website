b7934ffdbf85f7f01cf266c4ca5d4816647b8436
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-03-07 09:37:56 +0500

    updated on 07-Mar-23

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Main } from "next/document";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1 className="text-red-400">AOA</h1>
+      </main>
+    </>
+  );
+}
